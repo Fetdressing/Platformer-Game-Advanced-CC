3ebfe171906f23dd2c80dff5d9dd4a64d93fde19
Fetdressing <simon.home@hotmail.com>
2017-02-27 16:38:43 +0100

    meck med mood och miljö

---
+++ ProjectPlatformGrappling/Assets/OurStuff/Scripts/VisualEffects/FollowObj.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObj : MonoBehaviour {
+    public Transform followObj;
+    public Vector3 offsetV = Vector3.zero;
+    public float followSpeed = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, followObj.position + offsetV, followSpeed * Time.deltaTime);
+	}
+}
