0bb457eb13d32ea11696cc08d4ac824fa31218ec
narkos <narkos.newb@gmail.com>
2017-03-01 21:49:50 +0100

    Crazy fog inlagd. Känns weird

---
+++ ProjectPlatformGrappling/Assets/OurStuff/Prefabs/VisualsEffects/LayeredFog/NykrLayeredFog.cs
@@ -0,0 +1,197 @@
+﻿using UnityEngine;
+using System;
+
+[ExecuteInEditMode]
+[RequireComponent (typeof(Camera))]
+[AddComponentMenu ("Nykr/NykrLayeredFog")]
+
+public class NykrLayeredFog : UnityStandardAssets.ImageEffects.PostEffectsBase
+{
+    
+    [Tooltip("Apply distance-based fog")]
+    public bool m_distanceFog = true;
+    public FogMode m_fogMode = FogMode.Linear;
+    [Range(0.0f, 0.1f)]
+    public float m_fogDensity = 0.01f;
+    public float m_fogStartDistance;
+    public float m_fogEndDistance;
+    [Tooltip("Exclude far plane pixeld from distance-based fog")]
+    public bool m_excludeFarPixels = true;
+    public bool m_useRadialDistance = false;
+    public bool m_heightFog = false;
+    public float m_height = 1.0f;
+    [Range(0.001f, 10.0f)]
+    public float m_heightDensity = 2.0f;
+    public float m_startDistance = 0.0f;
+    public Gradient m_gradient;
+
+    public Shader m_fogShader = null;
+    
+    private Material m_fogMaterial = null;
+    private Texture2D m_fogTexture = null;
+    //var sceneMode = RenderSettings.fogMode;
+    //var sceneDensity = RenderSettings.fogDensity;
+    //var sceneStart = RenderSettings.fogStartDistance;
+    //var sceneEnd = RenderSettings.fogEndDistance;
+
+
+    public override bool CheckResources()
+    {
+        CheckSupport(true);
+        m_fogMaterial = CheckShaderAndCreateMaterial(m_fogShader, m_fogMaterial);
+        if(!isSupported)
+        {
+            ReportAutoDisable();
+        }
+        return isSupported;
+    }
+    [ImageEffectOpaque]
+    void OnRenderImage(RenderTexture p_source, RenderTexture p_destination)
+    {
+        if(CheckResources()==false || (!m_distanceFog && !m_heightFog))
+        {
+            Graphics.Blit(p_source, p_destination);
+            return;
+        }
+        Camera cam = GetComponent<Camera>();
+        Transform camTransform = cam.transform;
+        float camNear = cam.nearClipPlane;
+        float camFar = cam.farClipPlane;
+        float camFov = cam.fieldOfView;
+        float camAspect = cam.aspect;
+
+        Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+        float fovWHalfv = camFov * 0.5f;
+
+        Vector3 toRight = camTransform.right * camNear * Mathf.Tan(fovWHalfv * Mathf.Deg2Rad) * camAspect;
+        Vector3 toTop = camTransform.up * camNear * Mathf.Tan(fovWHalfv * Mathf.Deg2Rad);
+        Vector3 topLeft = (camTransform.forward * camNear - toRight + toTop);
+        float camScale = topLeft.magnitude * camFar / camNear;
+
+        topLeft.Normalize();
+        topLeft *= camScale;
+
+        Vector3 topRight = (camTransform.forward * camNear + toRight + toTop);
+        topRight.Normalize();
+        topRight *= camScale;
+
+
+        Vector3 bottomRight = (camTransform.forward * camNear + toRight - toTop);
+        bottomRight.Normalize();
+        bottomRight *= camScale;
+
+        Vector3 bottomLeft = (camTransform.forward * camNear - toRight - toTop);
+        bottomLeft.Normalize();
+        bottomLeft *= camScale;
+
+        frustumCorners.SetRow(0, topLeft);
+        frustumCorners.SetRow(1, topRight);
+        frustumCorners.SetRow(2, bottomRight);
+        frustumCorners.SetRow(3, bottomLeft);
+
+        var camPos = camTransform.position;
+        float FdotC = camPos.y - m_height;
+        float paramk = (FdotC <= 0.0f ? 1.0f : 0.0f);
+        float excludeDepth = (m_excludeFarPixels ? 1.0f : 2.0f);
+        m_fogMaterial.SetMatrix("_FrustumCornersWS", frustumCorners);
+        m_fogMaterial.SetVector("_CameraWS", camPos);
+        m_fogMaterial.SetVector("_HeightParams", new Vector4(m_height, FdotC, paramk, m_heightDensity * 0.5f));
+        m_fogMaterial.SetVector("_DistanceParams", new Vector4(-Mathf.Max(m_startDistance, 0.0f), excludeDepth, 0.0f, 0.0f));
+
+        var sceneMode = m_fogMode;
+        var sceneDensity = (m_fogDensity * 0.1);
+        var sceneStart = m_fogStartDistance;
+        var sceneEnd = m_fogEndDistance;
+        
+        bool linear = (sceneMode == FogMode.Linear);
+        float diff = linear ? sceneEnd - sceneStart : 0.0f;
+        float invDiff = Mathf.Abs(diff) > 0.0001f ? 1.0f / diff : 0.0f;
+
+        Vector4 sceneParams;
+        sceneParams.x = (float)sceneDensity * 1.2011224087f; // density /sqrt(ln(2)), used by Exp2 fog mode
+        sceneParams.y = (float)sceneDensity * 1.4426950408f; // density / ln(2), used by Exp for mode
+        sceneParams.z = linear ? -invDiff : 0.0f;
+        sceneParams.w = linear ? sceneEnd * invDiff : 0.0f;
+
+        m_fogMaterial.SetVector("_SceneFogParams", sceneParams);
+
+        int gradientKeyCount = CreateTextureFromGradient(m_gradient);
+
+
+        m_fogMaterial.SetVector("_SceneFogMode", new Vector4((int)sceneMode, m_useRadialDistance ? 1 : 0, gradientKeyCount, sceneEnd));
+        m_fogMaterial.SetTexture("_GradientTexture", m_fogTexture);
+        
+
+        int pass = 0;
+        if (m_distanceFog && m_heightFog)
+            pass = 0;
+        else if (m_distanceFog)
+            pass = 1;
+        else
+            pass = 2;
+        CustomGraphicsBlit(p_source, p_destination, m_fogMaterial, pass);
+    }
+
+    int CreateTextureFromGradient(Gradient p_gradient)
+    {
+
+        //Create texture with the same size as the number of gradient keys
+        //Alpha channel fort each is populated with time to enable interpolation in shader
+        Matrix4x4 keyMatrix =  Matrix4x4.identity;
+        Vector4 row1 = Vector4.zero;
+        Vector4 row2 = Vector4.zero;
+        int keyCount = p_gradient.colorKeys.Length;
+        m_fogTexture = new Texture2D(100, 1);
+        for(int i = 0; i<100; i++)
+        {
+            float keyTime = i * .01f;
+            Color keyColor = p_gradient.Evaluate(keyTime);
+            m_fogTexture.SetPixel(i, 1, keyColor); 
+        }
+        m_fogTexture.filterMode = FilterMode.Bilinear;
+        //m_fogTexture.wrapMode = TextureWrapMode.Repeat;
+        m_fogTexture.Apply();
+
+        //keyMatrix.SetRow(1, row1);
+        //keyMatrix.SetRow(2, row2);
+        m_fogMaterial.SetVector("_GradientKeyTimes1", row1);
+        m_fogMaterial.SetVector("_GradientKeyTimes2", row2);
+        //m_fogMaterial.SetMatrix("_GradientKeyTimes", keyMatrix);
+
+
+        return keyCount;
+    }
+
+    static void CustomGraphicsBlit(RenderTexture p_source, RenderTexture p_dest, Material p_fxMaterial, int p_passNr)
+    {
+        RenderTexture.active = p_dest;
+
+        p_fxMaterial.SetTexture("_MainTex", p_source);
+        //p_fxMaterial.SetTexture("_GradientTexture", p_source);
+        //p_fxMaterial.SetTexture("_GradientTexture",)
+        GL.PushMatrix();
+        GL.LoadOrtho();
+
+        p_fxMaterial.SetPass(p_passNr);
+
+        GL.Begin(GL.QUADS);
+
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f); // BL
+
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f); // BR
+
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f); // TR
+
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f); // TL
+
+        GL.End();
+        GL.PopMatrix();
+
+
+    }
+}
---
+++ ProjectPlatformGrappling/Assets/OurStuff/Prefabs/VisualsEffects/LayeredFog/NykrLayeredFog.shader
@@ -0,0 +1,204 @@
+﻿Shader "NykrShaders/NykrLayeredFog"
+{
+	Properties
+	{
+		_MainTex("Base (RGB)", 2D) = "black" {}
+		_GradientTexture("Texture", 2D) = "white" {}
+	}
+
+	CGINCLUDE
+
+		#include "UnityCG.cginc"
+
+		uniform sampler2D _MainTex;
+		uniform sampler2D _GradientTexture;
+		uniform sampler2D_float _CameraDepthTexture;
+
+		// x = fog height
+		// y = FdotC (CameraY-FogHeight)
+		// z = k (FdotC > 0.0)
+		// w = a/2
+		uniform float4 _HeightParams;
+		uniform float4 _DistanceParams; //x = start distance
+
+		int4 _SceneFogMode; // x= fog mode, y = radial bool
+		float4 _SceneFogParams;
+#ifndef UNITY_APPLY_FOG
+		half4 unity_fogColor;	//Kanske är denna som skall ändras för att få mitt coola resultat
+		//half4 unity_fogDensity; 
+#endif
+		uniform float4 _MainTex_TexelSize;
+		uniform float4x4 _FrustumCornersWS;
+		uniform float4 _CameraWS;
+
+		struct v2f 
+		{
+			float4 pos : SV_POSITION;
+			float2 uv : TEXCOORD0;
+			float2 uv_depth : TEXCOORD1;
+			float4 interpolatedRay : TEXCOORD2;
+		};
+
+		v2f vert(appdata_img v)
+		{
+			v2f output;
+			half index = v.vertex.z;
+			v.vertex.z = 0.1;
+			output.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+			output.uv = v.texcoord.xy;
+			output.uv_depth = v.texcoord.xy;
+
+#if UNITY_UV_STARTS_AT_TOP
+			if (_MainTex_TexelSize.y < 0)
+			{
+				output.uv.y = 1 - output.uv.y;
+			}
+#endif
+			output.interpolatedRay = _FrustumCornersWS[(int)index];
+			output.interpolatedRay.w = index;
+			return output;
+		}
+
+		//Applies one of standard fog formulas, given fog coordinate (i.e. distance)
+		half ComputeFogFactor(float p_coord)
+		{
+			float fogFac = 0.0f;
+			if (_SceneFogMode.x == 1)//Linead
+			{
+				// factor = (end-z)/(end-start) = z * (-1/(end-start)) + (end/(end-start))
+				fogFac = p_coord * _SceneFogParams.z + _SceneFogParams.w;
+			}
+			if (_SceneFogMode.x == 2)// exponential
+			{
+				// factor = exp(-density*z)
+				fogFac = _SceneFogParams.y * p_coord;
+				fogFac = exp2(-fogFac);
+			}
+			if (_SceneFogMode.x == 3) // exponential2
+			{
+				// factor = exp(-(density*z)^2)
+				fogFac = _SceneFogParams.x * p_coord;
+				fogFac = exp2(-fogFac*fogFac);
+			}
+			return saturate(fogFac);
+		}
+
+		float4 CalculateColor(float p_depth)
+		{
+			float colorBit = 1.0f / _SceneFogMode.z;
+			float depthPercentage = p_depth / _SceneFogMode.w;
+			float2 coord = float2(saturate(depthPercentage), 0.5f);
+			float4 color = tex2D(_GradientTexture, coord);
+			color.w = 1.0f;
+			return color;
+		}
+
+		//Distance based fog
+		float ComputeDistance(float3 p_camdir, float p_zdepth)
+		{
+			float distance;
+			if (_SceneFogMode.y == 1)
+			{
+				distance = length(p_camdir);
+			}
+			else
+			{
+				distance = p_zdepth * _ProjectionParams.z;
+			}
+			// Built-in fog starts at near plane, so match that by
+			// subtracting the near value. Not a perfect approximation
+			// if near plane is very large, but good enough.
+			distance -= _ProjectionParams.y;
+			return distance;
+		}
+		// Linear half-space fog, from https://www.terathon.com/lengyel/Lengyel-UnifiedFog.pdf
+		float ComputeHalfSpace(float3 p_wsDir)
+		{
+			float3 wPos = _CameraWS + p_wsDir;
+			float FH = _HeightParams.x;
+			float3 C = _CameraWS;
+			float3 V = p_wsDir;
+			float3 P = wPos;
+			float3 aV = _HeightParams.w * V;
+			float FdotC = _HeightParams.y;
+			float k = _HeightParams.z;
+			float FdotP = P.y - FH;
+			float FdotV = p_wsDir.y;
+			float c1 = k * (FdotP + FdotC);
+			float c2 = (1 - 2 * k)*FdotP;
+			float g = min(c2, 0.0);
+			g = -length(aV) * (c1 - g * g / abs(FdotV + 1.0e-5f));
+			return g;
+		}
+		half4 ComputeFog(v2f i, bool distance, bool height) : SV_TARGET
+		{
+			half4 sceneColor = tex2D(_MainTex, i.uv);
+
+			//Reconstruct worldspace position and direction towards this screen pixel
+			float rawDepth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv_depth);
+			float dpth = Linear01Depth(rawDepth);
+			float4 wsDir = dpth * i.interpolatedRay;
+			float4 wsPos = _CameraWS + wsDir;
+
+			// Compute fog distance
+			float g = _DistanceParams.x;
+			if (distance)
+			{
+				g += ComputeDistance(wsDir, dpth);
+			}
+			if (height)
+			{
+				g += ComputeHalfSpace(wsDir);
+			}
+
+			// Compute fog amount
+			half fogFac = ComputeFogFactor(max(0.0, g));
+			// Do not fog skybox
+			if (rawDepth == _DistanceParams.y)
+			{
+				fogFac = 1.0f;
+			}
+
+			float4 color = CalculateColor(g);
+			return lerp(color, sceneColor, fogFac);
+		}
+		
+
+ENDCG
+
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always Fog { Mode Off }
+
+
+			// 0: distance + height
+			Pass
+		{
+			CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+			half4 frag(v2f i) : SV_Target{ return ComputeFog(i, true, true); }
+			ENDCG
+		}
+			// 1: distance
+			Pass
+		{
+			CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+			half4 frag(v2f i) : SV_Target{ return ComputeFog(i, true, false); }
+			ENDCG
+		}
+			// 2: height
+			Pass
+		{
+			CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+			half4 frag(v2f i) : SV_Target{ return ComputeFog(i, false, true); }
+			ENDCG
+		}
+	}
+		Fallback off
+}
