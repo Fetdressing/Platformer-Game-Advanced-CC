cc3faf7e1f11dbc19a5e8ed291c5e4f9a5090e6c
Fetdressing <simon.home@hotmail.com>
2017-03-20 17:18:59 +0100

    fixad mainmenu

--- ProjectPlatformGrappling/Assets/OurStuff/Scripts/Game/MainMenu.cs
+++ ProjectPlatformGrappling/Assets/OurStuff/Scripts/Game/MainMenu.cs
@@ -1,33 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class MainMenu : MonoBehaviour {
     LevelManager levelManager;
 
     private string selectedScene = "Scene1SpiritWorld";
+
+    public MenuPage[] menues;
+
+    public string startCanvas;
+    public GameObject levelSelectionCanvas;
 	// Use this for initialization
 	void Start () {
         levelManager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
 	}
 
 
-    public void StartGame(string sceneName)
+    public void OpenMenu(string menuName)
     {
-        StartGame();
-        
-        //LevelLoader lLoader = FindObjectOfType(typeof(LevelLoader)) as LevelLoader;
-        //lLoader.LoadLevel(sceneName);
-    }
+        for(int i = 0; i < menues.Length; i++)
+        {
+            if(string.Equals(menues[i].name, menuName))
+            {
+                menues[i].item.SetActive(true);
+            }
+            else
+            {
+                menues[i].item.SetActive(false);
+            }
+        }
 
-    public void StartGame()
-    {
-        LevelLoader lLoader = FindObjectOfType(typeof(LevelLoader)) as LevelLoader;
-        lLoader.LoadLevel(levelManager.standardLevels[0]);
     }
 
     public void Exit()
     {
         Application.Quit();
+    }
+
+
+    [System.Serializable]
+    public struct MenuPage
+    {
+        public string name;
+        public GameObject item;
     }
 }
